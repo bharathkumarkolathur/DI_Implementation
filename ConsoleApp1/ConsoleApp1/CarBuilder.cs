d4b3daef0fb0820435ea2422969be1a3cc60fcf4
bharathkumarkolathur <bharathkumar.kolathur@gmail.com>
2017-11-19 21:52:42 -0500

    Intial Commit for DI implementation

---
+++ ConsoleApp1/ConsoleApp1/CarBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CarBuilder : ICarBuilder
+    {
+        private readonly ICar _carA;
+        private readonly ICar _carB;
+        private readonly ICar _carC;
+
+        public CarBuilder(ICar carA, ICar carB, ICar carC)
+        {
+            _carA = carA;
+            _carB = carB;
+            _carC = carC;
+
+        }
+
+        public void BuildAllCars()
+        {
+            _carA.Build();
+            _carB.Build();
+            _carC.Build();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Injection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<ICar, CarA>("a");
+            container.RegisterType<ICar, CarB>("b");
+            container.RegisterType<ICar, CarC>("c");
+
+           container.RegisterType<CarBuilder>(new InjectionConstructor(
+                new  ResolvedParameter<ICar>("a"),
+                new  ResolvedParameter<ICar>("b"),
+                new ResolvedParameter<ICar>("c")
+                ));
+
+            container.RegisterType<ICarBuilder, CarBuilder>();
+
+            var carBuilder= container.Resolve<ICarBuilder>();
+
+            carBuilder.BuildAllCars();
+
+            Console.ReadLine();
+
+        }
+    }
+}
